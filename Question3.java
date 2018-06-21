8a7f667c062eb07cd915a12478ede4812122cc4b
Sangeetha Sasikumar <sangeekumar7@gmail.com>
2018-06-20 22:32:19 -0400

    Create Question3.java

---
+++ Question3.java
@@ -0,0 +1,5 @@
+Hi, I wasn't able to solve this one because I only found about this fellowship at the last minute. 
+But, an appraoch I am thinking of is to use a for loop and start with i as 1, then we ask ourselves,
+what is the smallest value you can use to make for using "1s", and we increment that 1 until we reach 
+the value we need, in the situation provided, it is 4. We will continue slowly increasing this value of
+i. 
